0043da184a24179111f02bca71cefd747dcf139a
nilsreichardt <nils@sharezone.net>
2021-07-13 14:57:30 +0200

    test with matchmaking methods

--- test/tzdatetime_to_datetime_test.dart
+++ test/tzdatetime_to_datetime_test.dart
@@ -1,28 +1,107 @@
+import 'package:cron_parser/cron_parser.dart';
 import 'package:test/test.dart';
 import 'package:timezone/standalone.dart';
 
-Future<void> main() async {
-  await initializeTimeZone();
+void main() {
+  group('MatchmakingCubit', () {
+    group('.calcMatchmakingEndtime()', () {
+      test('5h', () {
+        expect(
+            calcMatchmakingEndtime(
+                DateTime(2021, 1, 1), const Duration(hours: 5)),
+            DateTime(2021, 1, 1, 5));
+      });
 
-  const timezone = "Europe/Berlin";
+      test('1d', () {
+        expect(
+            calcMatchmakingEndtime(
+                DateTime(2021, 1, 1), const Duration(days: 1)),
+            DateTime(2021, 1, 2));
+      });
+    });
 
-  test(
+    group('.calcMatchmakingStart())', () {
+      const timezone = "Europe/Berlin";
+
+      test(
+          // ignore: lines_longer_than_80_chars
+          "get previous cron time, if matchmaking already starting and is still going",
+          () {
+        final current = DateTime.parse('2019-11-23 12:00:00');
+        final expected = DateTime.parse('2019-11-23 11:00:00');
+        const duration = Duration(hours: 2);
+        expect(
+          calcMatchmakingStart(
+            current,
+            Cron().parse(
+              '0 11 * * *',
+              timezone,
+              TZDateTime.from(
+                  current, TZDateTime.now(getLocation(timezone)).location),
+            ),
+            duration,
+          ),
+          expected,
+        );
+      });
+
+      test('get next cron time, if matchmaking ended', () {
+        final current = DateTime.parse('2019-11-23 13:00:01');
+        final expected = DateTime.parse('2019-11-24 11:00:00');
+        const duration = Duration(hours: 2);
+        expect(
+          calcMatchmakingStart(
+            current,
+            Cron().parse(
+              '0 11 * * *',
+              timezone,
+              TZDateTime.from(
+                  current, TZDateTime.now(getLocation(timezone)).location),
+            ),
+            duration,
+          ),
+          expected,
+        );
+      });
+
       // ignore: lines_longer_than_80_chars
-      'casting test', () {
-    final current = DateTime.parse('2021-07-11 13:00:01');
-    final expected = DateTime.parse('2021-07-11 13:00:01');
-    expect(
-      cast(
-        TZDateTime.from(
-            current, TZDateTime.now(getLocation(timezone)).location),
-      ),
-      expected,
-    );
+      // Test for bug "app shoes matchmaking is started on 1. August, even if cron job is set to 7. August":
+      // https://github.com/GatchHQ/gatch/issues/479
+      test(
+          // ignore: lines_longer_than_80_chars
+          'should start 7. August, if 7. August is set in the cron (0 19 7 8 *)',
+          () {
+        final current = DateTime.parse('2021-07-11 13:00:01');
+        final expected = DateTime.parse('2021-08-07 19:00:00');
+        const duration = Duration(hours: 2);
+        expect(
+          calcMatchmakingStart(
+            current,
+            Cron().parse(
+              '0 19 7 8 *',
+              timezone,
+              TZDateTime.from(
+                  current, TZDateTime.now(getLocation(timezone)).location),
+            ),
+            duration,
+          ),
+          expected,
+        );
+      });
+    });
   });
 }
 
-DateTime cast(
-  TZDateTime cron,
-) {
-  return cron;
+// ignore: lines_longer_than_80_chars
+DateTime calcMatchmakingStart(
+    DateTime current, CronIterator<TZDateTime> cron, Duration duration) {
+  final previous = cron.previous();
+  if (previous.add(duration).isAfter(current)) {
+    return previous;
+  }
+  return cron.next();
+}
+
+DateTime calcMatchmakingEndtime(DateTime start, Duration duration) {
+  return start.add(duration);
 }
